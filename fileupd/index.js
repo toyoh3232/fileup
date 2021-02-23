e5978a33344d0b7b8ee338ca2b041514e0d2979e
Toyoharu Han <toyoharu.han@zohomail.com>
2021-02-24 02:56:50 +0900

    add list

--- fileupd/index.js
+++ fileupd/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const multer = require("multer");
-const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const port = 5432;
-const uploadpath = ".uploads";
+const uploadpath = "uploads";
+const uuidpattern = "[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -13,12 +14,31 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         console.log(file);
-        cb(null, uuidv4() + "_" + file.originalname);
+        cb(null, uuidv4() + file.originalname);
     }
 });
 
 app.get("/list", (req, res) => {
-    res.status(200).json("TODO");
+    fs.readdir(uploadpath, (err, files) => {
+        if (err)
+            // TODO
+            console.log(err);
+        else {
+            res.status(200).json({data: files.reduce((acc, cur) => {
+                if (cur.match(uuidpattern)) {
+                    var uuid = cur.substr(0,36);
+                    var filename = cur.substr(36);
+                    acc[uuid] = filename;
+                }
+                return acc;
+            },{}),
+            meta: {
+                msg : "ok",
+                error : "false",
+                status : 200
+            }});
+        }
+    });
 });
 
 app.post("/upload", multer({ storage:storage }).single('file'), (req, res) => {
