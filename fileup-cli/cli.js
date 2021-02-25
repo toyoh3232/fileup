f92193059f7d3fb726a6509b222e54dec4b67e4f
Toyoharu Han <toyoharu.han@zohomail.com>
2021-02-26 00:04:30 +0900

    finish delete

--- fileup-cli/cli.js
+++ fileup-cli/cli.js
@@ -1,41 +1,70 @@
 #!/usr/bin/env node
-const { program } = require('commander');
-const got = require('got');
-const { Select } = require('enquirer');
+const { program } = require("commander");
+const got = require("got");
+const { Select } = require("enquirer");
 
-program.version('0.0.1');
+program.version("0.0.1");
 
 program
-  .command('list')
-  .description('list all uploaded files')
+  .command("list")
+  .description("list all uploaded files")
   .action(() => {
-    got("http://127.0.0.1:5432/listmore",{responseType: 'json'}).then(({ body }) => {
+    got("http://127.0.0.1:5432/list?detailed=true", { responseType: "json" })
+      .then(({ body }) => {
         if (body.meta.error) {
-            console.log(body.meta.msg);
+          console.log(body.meta.msg);
         } else {
-            for (uuid in body.data) {
-                console.log(`${body.data[uuid].updated_date} ${body.data[uuid].filename}`);
-            } 
+          for (uuid in body.data) {
+            console.log(
+              `${body.data[uuid].uploaddate} ${body.data[uuid].filename}`
+            );
+          }
         }
-      }).catch((err) => {
+      })
+      .catch((err) => {
         console.log(err);
       });
   });
 
 program
-  .command('del <filename>')
-  .description('delete file')
-  .action(() =>{
-    var choices = [{name:'apple', value:1},{name:'grape',value:2}];
-    var prompt = new Select({
-        name: 'color',
-        message: 'Pick a flavor',
-        choices: choices
+  .command("delete <filename>")
+  .description("delete file")
+  .action((filename) => {
+    got(`http://127.0.0.1:5432/list?detailed=true&filename=${filename}`, {
+      responseType: "json",
+    })
+      .then(({ body }) => {
+        if (body.meta.error) throw body.meta.msg;
+        if (Object.keys(body.data).length == 0) throw "error: file not found";
+        if (Object.keys(body.data).length == 1)
+          return Object.keys(body.data)[0];
+        var choices = [];
+        for (uuid in body.data) {
+          choices.push({
+            name: `${body.data[uuid].filename} upload at ${body.data[uuid].uploaddate}`,
+            value: uuid,
+          });
+        }
+        var prompt = new Select({
+          message:
+            "Oops, we have many files with the same name. which do you want to delete?",
+          choices: choices,
+        });
+        return prompt
+          .run()
+          .then((answer) => choices.find((x) => x.name === answer).value);
+      })
+      .then((uuid) => {
+        return got(`http://127.0.0.1:5432/delete?uuid=${uuid}`, {
+          responseType: "json",
+        }).then(({ body }) => body.meta.msg);
+      })
+      .then((msg) => {
+        console.log(msg);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-      prompt.run().then(answer => console.log('Answer:', choices.find(x => x.name === answer).value))
-      .catch(console.error);
-      console.log('delete');
   });
 
 program.parse(process.argv);
-
--- fileupd/index.js
+++ fileupd/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const multer = require("multer");
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const sanitize = require("sanitize-filename");
 
 const app = express();
@@ -12,169 +12,172 @@ const maxfilename = 255 - path.resolve(uploadpath).length - 36 - 1;
 const uuidpattern = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/;
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadpath);
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        if (sanitize(file.originalname) !== file.originalname)
-            cb (new Error("filename contains illegal characters"), "illegal characters")
-        else if (path.join(uploadpath,file.originalname).length > 255)
-            cb (new Error(`filename is too long, must equal or less than ${maxfilename}`), "if file name too long")
-        else
-            cb(null, uuidv4() + file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadpath);
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    if (sanitize(file.originalname) !== file.originalname)
+      cb(
+        new Error("filename contains illegal characters"),
+        "illegal characters"
+      );
+    else if (path.join(uploadpath, file.originalname).length > 255)
+      cb(
+        new Error(
+          `filename is too long, must equal or less than ${maxfilename}`
+        ),
+        "if file name too long"
+      );
+    else cb(null, uuidv4() + file.originalname);
+  },
 });
 
 const option = {
-    storage:storage, 
-    limits: {
-        files: 1,
-        fileSize: 4*1024*1024*1024,
-        
-    }};
-const uploader = multer(option).single('file');
+  storage: storage,
+  limits: {
+    files: 1,
+    fileSize: 4 * 1024 * 1024 * 1024,
+  },
+};
+const uploader = multer(option).single("file");
 
 app.get("/list", (req, res) => {
-    fs.readdir(uploadpath, (err, files) => {
-        if (err)
-            res.status(400).json({
-                meta: {
-                    msg: err.message,
-                    error: true,
-                    status: 400
-                }});
-        else {
-            res.status(200).json({data: files.reduce((acc, cur) => {
-                var indexes = uuidpattern.exec(cur);
-                if (indexes && indexes.index == 0) {
-                    var uuid = cur.substr(0,36);
-                    var filename = cur.substr(36);
-                    acc[uuid] = filename;
-                }
-                return acc;
-            },{}),
-            meta: {
-                msg : "ok",
-                error : false,
-                status : 200
-            }});
-        }
-    });
+  fs.readdir(uploadpath, (err, files) => {
+    if (err)
+      res.status(400).json({
+        meta: {
+          msg: `error: ${err.message}`,
+          error: true,
+          status: 400,
+        },
+      });
+    else {
+      res.status(200).json({
+        data: files.reduce((acc, cur) => {
+          var indexes = uuidpattern.exec(cur);
+          if (indexes && indexes.index == 0) {
+            var uuid = cur.substr(0, 36);
+            var filename = cur.substr(36);
+            if (req.query["filename"] && req.query["filename"] !== filename)
+              return acc;
+            if (req.query["detailed"] === "true")
+              acc[uuid] = {
+                filename: filename,
+                uploaddate: new Date(
+                  fs.statSync(path.join(uploadpath, cur)).birthtimeMs
+                ).toLocaleString(),
+              };
+            else acc[uuid] = filename;
+          }
+          return acc;
+        }, {}),
+        meta: {
+          msg: "ok",
+          error: false,
+          status: 200,
+        },
+      });
+    }
+  });
 });
-
-app.get("/listmore", (req, res) => {
-    fs.readdir(uploadpath, (err, files) => {
-        if (err)
-            res.status(400).json({
-                meta: {
-                    msg: err.message,
-                    error: true,
-                    status: 400
-                }});
-        else {
-            res.status(200).json({data: files.reduce((acc, cur) => {
-                var indexes = uuidpattern.exec(cur);
-                if (indexes && indexes.index == 0) {
-                    var uuid = cur.substr(0,36);
-                    var filename = cur.substr(36);
-                    acc[uuid] = {
-                        filename:filename, 
-                        updated_date: new Date(fs.statSync(path.join(uploadpath, cur)).birthtimeMs).toLocaleString()
-                    };
-                }
-                return acc;
-            },{}),
-            meta: {
-                msg : "ok",
-                error : false,
-                status : 200
-            }});
-        }
-    });
-});
-
 
 app.get("/delete", (req, res) => {
-    if (!req.query["uuid"]){
-        return res.status(400).json({meta: {
-            msg: "need parameter uuid",
-            error: true,
-            status: 400
-        }});
-    }
-    var uuid = req.query["uuid"];
-    if (!uuid.match(uuidpattern)) {
-        return res.status(400).json({meta: {
-            msg: "error format uuid",
-            error: true,
-            status: 400
-        }});
-    }
-    fs.readdir(uploadpath, (err, files) => {
-        if (err)
-            res.status(400).json({
-                meta: {
-                msg: err.message,
-                error: true,
-                status: 400
-            }});
-        else {
-            var filename = files.find(name => name.startsWith(uuid));
-            if (!filename) {
-                res.status(400).json({meta: {
-                    msg: "file not found",
-                    error: true,
-                    status: 400
-                }});
-            } else {
-                var fullname = path.join(uploadpath, filename);
-                var newfullname = path.join(uploadpath, `-${filename}`);
-                fs.rename(fullname,newfullname, (err) => {
-                    if (err) {
-                        res.status(400).json({meta: {
-                            msg: "delete file error",
-                            error: true,
-                            status: 400
-                        }});
-                    }
-                    else {
-                        res.status(200).json({meta: {
-                            msg: "ok",
-                            error: false,
-                            status: 200
-                        }});
-                    }
-                });
-            }
-        }
+  if (!req.query["uuid"]) {
+    return res.status(400).json({
+      meta: {
+        msg: "error: need parameter uuid",
+        error: true,
+        status: 400,
+      },
     });
+  }
+  var uuid = req.query["uuid"];
+  if (!uuid.match(uuidpattern)) {
+    return res.status(400).json({
+      meta: {
+        msg: "error: invalid format uuid",
+        error: true,
+        status: 400,
+      },
+    });
+  }
+  fs.readdir(uploadpath, (err, files) => {
+    if (err)
+      res.status(400).json({
+        meta: {
+          msg: `error: ${err.message}`,
+          error: true,
+          status: 400,
+        },
+      });
+    else {
+      var filename = files.find((name) => name.startsWith(uuid));
+      if (!filename) {
+        res.status(400).json({
+          meta: {
+            msg: "error: file not found",
+            error: true,
+            status: 400,
+          },
+        });
+      } else {
+        var fullname = path.join(uploadpath, filename);
+        var newfullname = path.join(uploadpath, `-${filename}`);
+        fs.rename(fullname, newfullname, (err) => {
+          if (err) {
+            res.status(400).json({
+              meta: {
+                msg: "error: deletion failed",
+                error: true,
+                status: 400,
+              },
+            });
+          } else {
+            res.status(200).json({
+              meta: {
+                msg: "ok",
+                error: false,
+                status: 200,
+              },
+            });
+          }
+        });
+      }
+    }
+  });
 });
 
 app.post("/upload", (req, res) => {
-    uploader(req, res, (err) => {
-        if (err) {
-            res.status(400).json({meta: {
-                msg: err.message,
-                error: true,
-                status: 400
-            }});
-        } else if (!req.file) {
-            res.status(400).json({meta: {
-                msg: "no multpart data",
-                error: true,
-                status: 400
-            }});
-        } else {
-            res.status(200).json({meta: {
-                msg: "ok",
-                error: false,
-                status: 200
-            }});
-        }
-    });
+  uploader(req, res, (err) => {
+    if (err) {
+      res.status(400).json({
+        meta: {
+          msg: err.message,
+          error: true,
+          status: 400,
+        },
+      });
+    } else if (!req.file) {
+      res.status(400).json({
+        meta: {
+          msg: "no multpart data",
+          error: true,
+          status: 400,
+        },
+      });
+    } else {
+      res.status(200).json({
+        meta: {
+          msg: "ok",
+          error: false,
+          status: 200,
+        },
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
-    console.log(`listen on ${port}`);
+  console.log(`listen on ${port}`);
 });
